b639c6afb51eace3ee074b0c05234a12b60b235a
Manhhoclaptrinh <147614938+Manhhoclaptrinh@users.noreply.github.com>
2026-01-04 19:12:09 +0700

    Add circle calculations in 03_operators.php
    Calculate the circumference and area of a circle.

---
+++ PHP/Lab02/BaiTapTrenLop/03_operators.php
@@ -0,0 +1,12 @@
+<?php
+$r = 5;
+$pi = 3.14;
+
+$chuvi = 2* $pi * $r;
+$dientich = $pi * $r * $r;
+
+echo"<h3> Tính hình tròn </h3>";
+echo"Bán kính : $r <br>";
+echo"Chu vi : $chuvi <br>";
+echo"Diện tích : $dientich <br>";
+?>
