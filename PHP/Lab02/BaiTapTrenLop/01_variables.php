15e2611b71b8829926e9a0ce2aa51b505d6cc993
Manhhoclaptrinh <147614938+Manhhoclaptrinh@users.noreply.github.com>
2026-01-04 19:11:25 +0700

    Create 01_variables.php

---
+++ PHP/Lab02/BaiTapTrenLop/01_variables.php
@@ -0,0 +1,10 @@
+<?php
+$name ="Đoàn Quang Mạnh";
+$class = "CNTT 14.2";
+$email = "doanmanh152005@gmail.com";
+
+echo"<h3> Thông tin cá nhân </h3>";
+echo"Tên: $name <br>";
+echo"Lớp: $class <br>";
+echo"Tên: $email <br>";
+?>
