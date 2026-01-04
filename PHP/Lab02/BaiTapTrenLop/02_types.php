30931642bd34aae3d6192e10b0a2c4d182fe2882
Manhhoclaptrinh <147614938+Manhhoclaptrinh@users.noreply.github.com>
2026-01-04 19:11:48 +0700

    Add PHP script to demonstrate data types

---
+++ PHP/Lab02/BaiTapTrenLop/02_types.php
@@ -0,0 +1,13 @@
+<?php 
+$a = 10;
+$b = 3.14;
+$c = "Hello_PHP";
+$d = true;
+$e = [1,2.3];
+
+echo "<h3>Các kiểu dữ liệu trong php</h3>";
+var_dump($a); echo "<br>";
+var_dump($b); echo "<br>";
+var_dump($c); echo "<br>";
+var_dump($e); echo "<br>";
+?> 
