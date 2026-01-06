56b27252d725700f43a77eabcdcd3814bf25270a
Manhhoclaptrinh <147614938+Manhhoclaptrinh@users.noreply.github.com>
2026-01-06 17:55:40 +0700

    Create Book class with properties and methods

---
+++ PHP/Lab04/ex6/Book.php
@@ -0,0 +1,35 @@
+<?php
+
+class Book
+{
+    private $id;
+    private $title;
+    private $qty;
+
+    public function __construct($id, $title, $qty)
+    {
+        $this->id = $id;
+        $this->title = $title;
+        $this->qty = (int)$qty;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function getQty()
+    {
+        return $this->qty;
+    }
+
+    public function getStatus()
+    {
+        return $this->qty > 0 ? 'Available' : 'Out of stock';
+    }
+}
