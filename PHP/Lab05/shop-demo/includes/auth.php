7be17716507d16ee3dbeb901a9fbbd3750108b30
Manhhoclaptrinh <147614938+Manhhoclaptrinh@users.noreply.github.com>
2026-01-08 22:15:12 +0700

    Create auth.php

---
+++ PHP/Lab05/shop-demo/includes/auth.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+function is_logged_in() {
+    return isset($_SESSION['user']);
+}
+
+function require_login() {
+    if (!is_logged_in()) {
+        header("Location: login.php");
+        exit();
+    }
+}
+
+function current_user() {
+    return $_SESSION['user'] ?? null;
+}
