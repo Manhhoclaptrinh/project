3b706443f755d772e53d3d6f1aa6dbb82c5bbbbe
Manhhoclaptrinh <147614938+Manhhoclaptrinh@users.noreply.github.com>
2026-01-18 22:36:54 +0700

    Create layout.php

---
+++ PHP/Lab09/project/app/views/layout.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Quản lý sinh viên</title>
+
+    <link rel="stylesheet" href="/labs/lab09/project/public/assets/css/style.css">
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+</head>
+<body>
+
+<?php require __DIR__ . '/' . $view . '.php'; ?>
+
+<script src="/labs/lab09/project/public/assets/js/app.js"></script>
+
+</body>
+</html>
