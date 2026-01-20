f5d96ca3434e08a0fa34c0635ebd73055df90f40
Manhhoclaptrinh <147614938+Manhhoclaptrinh@users.noreply.github.com>
2026-01-20 19:00:59 +0700

    Create create.php

---
+++ PHP/Lab10/app/Views/customers/create.php
@@ -0,0 +1,14 @@
+<h2>Thêm khách hàng</h2>
+
+<form method="post" action="index.php?controller=customer&action=store">
+    Họ tên:
+    <input type="text" name="full_name" required><br><br>
+
+    Email:
+    <input type="email" name="email"><br><br>
+
+    Điện thoại:
+    <input type="text" name="phone"><br><br>
+
+    <button type="submit">Lưu</button>
+</form>
