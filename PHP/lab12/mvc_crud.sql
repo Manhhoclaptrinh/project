e4e8b9490d340ded43eeb0d47078f68df35808ac
Manhhoclaptrinh <147614938+Manhhoclaptrinh@users.noreply.github.com>
2026-01-25 17:54:32 +0700

    Create mvc_crud.sql

---
+++ PHP/lab12/mvc_crud.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE mvc_crud CHARACTER SET utf8mb4;
+USE mvc_crud;
+
+CREATE TABLE categories (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  status TINYINT DEFAULT 1,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO categories(name) VALUES
+('Kitchen Tools'),
+('Appliances'),
+('Health & Care');
